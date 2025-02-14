263680b6d7577653e0232d728e545fe3cc5e378b
shainyachary <shainy.achary@gmail.com>
2025-02-14 23:24:41 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://user-auth-api-eight.vercel.app/"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = "https://user-auth-api-eight.vercel.app";
 
 export const registerUser = async (formData) =>
   axios.post(`${API_URL}/register`, formData);
